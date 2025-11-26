985ed3e92e1dea2ec497a4ee6032f9dbbb15e564
Andrea Hernández Bustos <ktmandre280@gmail.com>
2025-11-26 16:23:10 -0500

    Add files via upload

---
+++ Marca de Vehiculos/MARCAS DE AUTOS/app.js
@@ -0,0 +1,60 @@
+const toppings = document.getElementsByClassName('topping');
+
+for (const topping of toppings) {
+
+    
+    topping.addEventListener('mouseenter', () => {
+        topping.classList.add('hover-color');
+    });
+
+  
+    topping.addEventListener('mouseleave', () => {
+        topping.classList.remove('hover-color');
+    });
+
+
+    topping.addEventListener('click', () => {
+
+   
+        for (const t of toppings) {
+            t.classList.remove('selected-color');
+        }
+
+      
+        topping.classList.add('selected-color');
+
+        console.log(topping.innerText);
+    });
+}
+
+
+
+document.getElementById("Ferrari").addEventListener("click", () => {
+
+    document.body.style.backgroundImage = "url('Imagenes/Ferrari.png')";
+
+});
+
+
+
+document.getElementById("Lamborghini").addEventListener("click", () => {
+
+    document.body.style.backgroundImage = "url('Imagenes/Lamborghini.png')";
+
+});
+
+
+
+document.getElementById("BMW").addEventListener("click", () => {
+
+    document.body.style.backgroundImage = "url('Imagenes/BMW.png')";
+
+});
+
+
+
+document.getElementById("Mercedes").addEventListener("click", () => {
+
+    document.body.style.backgroundImage = "url('Imagenes/Mercedes.png')";
+
+});
